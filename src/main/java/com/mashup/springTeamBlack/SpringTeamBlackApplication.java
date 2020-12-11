8651712083d26ca4b9844d5bdbbd59bf2ff4740b
Yangsiyoung <ysjleader@gmail.com>
2020-12-11 22:53:23 +0900

    init

---
+++ src/main/java/com/mashup/springTeamBlack/SpringTeamBlackApplication.java
@@ -0,0 +1,13 @@
+package com.mashup.springTeamBlack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTeamBlackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTeamBlackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mashup/springTeamBlack/SpringTeamBlackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mashup.springTeamBlack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTeamBlackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
